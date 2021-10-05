90c75884731ef8dbffef7498c65554c922e70f0f
Zavier <zaviercassidy09@gmail.com>
2021-10-06 08:11:42 +1100

    Removed a switch statement that could be replaced with an int

--- src/mapList.cpp
+++ src/mapList.cpp
@@ -11,18 +11,7 @@ MapList::~MapList()
 
 void MapList::renderCurrentMap()
 {
-  switch(lvl)
-  {
-    case 0:
-      maps[0].render();
-      break;
-    case 1:
-      maps[1].render();
-      break;
-    case 2:
-      maps[2].render();
-      break;
-  } //switch(lvl)
+    maps[lvl].render();
 } //renderCurrentMap()
 
 int MapList::move(int direction, int& money)
